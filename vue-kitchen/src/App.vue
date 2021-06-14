60ef06bdb65369291faa051f529a5af2643cf098
Nicolas Nunes <n.nunes@testamento.fr>
2021-06-14 11:55:32 +0200

    create first view component

--- vue-kitchen/src/App.vue
+++ vue-kitchen/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navigation />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+html, body {
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 62.5%;
+  color: #111;
+  background: #fafafa;
 }
 </style>
---
+++ vue-kitchen/src/components/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="nav">
+        <img alt="Vue logo" height='50' src="../assets/logo.png">
+        <button class="nav--action" >Button</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 2.3rem 0;
+        border-bottom: 1px solid #D8D8D8;
+    }
+
+    .nav--action {
+        all: unset;
+        cursor: pointer;
+        padding: 1.3rem 2.3rem;
+        border-radius: 0.5rem;
+        background: #111;
+        color: white;
+        font-size: 1.3rem;
+        font-weight: 500;
+    }
+
+</style>
